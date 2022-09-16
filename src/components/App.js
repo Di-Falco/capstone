7e7769adcf95872857fecd878f89e32277ac6964
Anthony DiFalco <aodifalco@gmail.com>
2022-09-16 14:35:19 -0700

    add nav bar dropdowns in the header

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import './../App.css';
 import Header from './Header';
 import Home from './Home/Home';
-// import FilmControl from './Film/FilmControl';
+import FilmControl from './Film/FilmControl';
+import { BrowserRouter as Router, Routes, Route,  } from 'react-router-dom';
 
 function App() {
   return(
     <React.Fragment>
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/movies" element={<FilmControl />} />
+        </Routes>
+      </Router>
     </React.Fragment>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function Header() {
   return(
     <React.Fragment>
-      <h1>H1 Movies</h1>
+      <nav class="navbar sticky-top navbar-expand-lg justify-content-start">
+        <h1>H1 MOVIE RENTAL</h1>
+        <ul><li>
+          <Link to="/" class="header">Home</Link> 
+        </li></ul>
+        <ul><li>
+          <a class="header">Movies</a>
+          <ul>
+            <li><Link to="/movies">Browse All Movies</Link></li>
+          </ul>
+        </li></ul>
+      </nav>
     </React.Fragment>
   );
 }
