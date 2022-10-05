a023404f66e0cc33d08fe392acc103e030ee7bee
Anthony DiFalco <aodifalco@gmail.com>
2022-10-05 15:44:44 -0700

    add footer and adjust styles to accommodate

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer () {
   return (
     <React.Fragment>
       <div className="footer">
-        <span className="footer-text"><h4>{auth.currentUser? `Signed in as ${auth.currentUser.email}` : "Signed Out"}&emsp;. . .&emsp;Call ahead at 1 (800) 555-9098&emsp;. . .&emsp;visit us at 316 N Sunset Blvd&emsp;. . .&emsp;{auth.currentUser? `Signed in as ${auth.currentUser.email}` : "Signed Out"}&emsp;. . .&emsp;Call ahead at 1 (800) 555-9098&emsp;. . .&emsp;visit us at 316 N Sunset Blvd&emsp;. . .&emsp;</h4></span>
+        <span className="footer-text"><h4>&emsp;. . .&emsp;Call ahead at 1 (800) 555-0123&emsp;. . .&emsp;visit us at 316 N Sunset Blvd&emsp;. . .&emsp;Call ahead at 1 (800) 555-0123&emsp;. . .&emsp;visit us at 316 N Sunset Blvd&emsp;. . .&emsp;</h4></span>
       </div>  
     </React.Fragment>
   );
