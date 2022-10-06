19a6f7dbb4d2f61ed71c4faa762fe876bf8f9513
Anthony DiFalco <aodifalco@gmail.com>
2022-10-05 18:23:34 -0700

    reformat footer to include crucial seinfeld reference

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer () {
   return (
     <React.Fragment>
       <div className="footer">
-        <span className="footer-text"><h4>&emsp;. . .&emsp;Call ahead at 1 (800) 555-0123&emsp;. . .&emsp;visit us at 316 N Sunset Blvd&emsp;. . .&emsp;Call ahead at 1 (800) 555-0123&emsp;. . .&emsp;visit us at 316 N Sunset Blvd&emsp;. . .&emsp;</h4></span>
+        <span className="footer-text"><h4>&emsp;. . .&emsp;1 (800) 555-FILM&emsp;. . .&emsp;316 N Sunset Blvd&emsp;. . .&emsp;1 (800) 555-FILM&emsp;. . .&emsp;316 N Sunset Blvd&emsp;. . .&emsp;1 (800) 555-FILM&emsp;. . .&emsp;316 N Sunset Blvd&emsp;. . .&emsp;1 (800) 555-FILM&emsp;. . .&emsp;316 N Sunset Blvd&emsp;. . .&emsp;1 (800) 555-FILM&emsp;. . .&emsp;316 N Sunset Blvd&emsp;. . .&emsp;1 (800) 555-FILM&emsp;. . .&emsp;316 N Sunset Blvd&emsp;. . .&emsp;</h4></span>
       </div>  
     </React.Fragment>
   );
